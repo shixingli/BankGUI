c2e348b6de3fbb8ad252888fd03d9afa83e86242
Amanda Durfee <amandadurfee@dhcp-wifi-8021x-155-41-74-171.bu.edu>
2019-07-25 13:35:20 -0400

    added txn attribute

--- Customer.java
+++ Customer.java
@@ -2,7 +2,8 @@ public class Customer extends User {
   Loan loan;
   Savings saveAcc;
   Checking checkAcc;
-  double currentValue;
+  double currentBalance;
+  String[] transactionHistory;
  
   // three different constructors dependent on 
 }
