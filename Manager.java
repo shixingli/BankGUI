29983bb451da0554bd021c3960c886715af34d1b
Louis Tannudin <ltannudin@gmail.com>
2019-07-26 12:05:14 -0400

    moved ManagerFrame into Manager and create temp methods for Manager

--- Manager.java
+++ Manager.java
@@ -1,3 +1,16 @@
 public class Manager extends User {
   // methods like pass in a customer and get their information using the customer's getting methods
+
+    private class ManagerFrame {// GUI for the Manager part.
+
+    }
+    public Manager(){
+
+    }
+
+    public Customer getCustomer(Customer c){
+        return null;
+    }
+
+
 }
