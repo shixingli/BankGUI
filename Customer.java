172991dacd82e0a457a27df9fb7de7371f53c261
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2019-07-25 22:27:09 -0400

    sleepy customer progress

--- Customer.java
+++ Customer.java
@@ -1,12 +1,38 @@
 public class Customer extends User {
   Loan loan;
-  Savings saveAcc;
-  Checking checkAcc;
-  double currentBalance;
+  Account[] accounts;
+  double totalBalance; // unnecessary?
   String[] transactionHistory;
+  boolean collateral;
+  
+  public Customer() {
+  }
  
   // three different constructors dependent on 
 
+  public Loan getLoan() {
+    return this.loan;
+  }
+  
+  public void setLoan(Loan moneyDue) {
+    this.loan = moneyDue;
+  }
+  
+  /* accesses the accounts for the customer and updates the total balance */
+  public void updateBalance() {
+    double bal;
+    for (Account account : this.accounts) {
+      bal += account.getBalance();
+    }
+    this.totalBalance = bal;
+  }
+  
+  /* returns the total balance for all accounts for a customer */
+  public double getTotalBalance() {
+    return this.totalBalance;
+  }
+  
+  
   // get balance
   // get transaction history
 
