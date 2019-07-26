ea8b7839e7cabe6e39247d859298867c39ce8556
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2019-07-26 10:56:53 -0400

    formatting updates

--- Customer.java
+++ Customer.java
@@ -1,28 +1,57 @@
+import java.util.*;
+
 public class Customer extends User {
   Loan loan;
-  Account[] accounts;
+  List<Account> accounts;
   double totalBalance; // unnecessary?
-  String[] transactionHistory;
-  boolean collateral; // maybe this should be info about what their collateral is ???
+  List<String> txnHistory;
+  boolean collateral; // maybe this should be info about what their collateral is ??? (like a property or business)
   
+  /* no arg */
   public Customer() {
+    this.totalBalance = 0;
+    this.collateral = false;
+  }
+  
+  /* adding a new account dependent on money in that account and if they can have a loan or not */
+  public Customer(Account accAdd, boolean canHaveLoan) {
+    this.totalBalance = accAdd.view_balance();
+    this.collateral = canHaveLoan;
+    this.accounts.add(accAdd);
+  }
+  
+ /* adding multiple accounts and if they can have a loan or not */ 
+  public Customer(List<Account> accounts, boolean canHaveLoan) {
+    double bal = 0;
+    for (Account account : accounts) {
+      this.accounts.add(account);
+      bal += account.view_balance();
+    }
+    
+    this.collateral = canHaveLoan;
   }
  
-  // three different constructors dependent on 
+  /* only needed if the ACCOUNT creation is dependent on the customer, if not the total amount is determined in the above constructor */
+  public Customer(List<Account> accounts, boolean canHaveLoan, double openAmount) {
+    this(accounts, canHaveLoan);
+    this.totalBalance = openAmount;
+  }
 
+  /* getter for Loan */
   public Loan getLoan() {
     return this.loan;
   }
   
+  /* setter for Loan */
   public void setLoan(Loan moneyDue) {
     this.loan = moneyDue;
   }
   
   /* accesses the accounts for the customer and updates the total balance */
   public void updateBalance() {
-    double bal;
+    double bal = 0;
     for (Account account : this.accounts) {
-      bal += account.getBalance();
+      bal += account.view_balance();
     }
     this.totalBalance = bal;
   }
@@ -32,11 +61,12 @@ public class Customer extends User {
     return this.totalBalance;
   }
   
+  /* returns all accounts belonging to customer */
+  public List<Account> getAccounts() {
+    return this.accounts;
+  }
   
-  // get balance
-  // get transaction history
-
-  // if null, throw new illegal argument exception that has to be CAUGHT
-  // get savingAccount
-  // get checking account
+  public List<String> getHistoryAllAcc() {
+    return this.txnHistory;
+  }
 }
