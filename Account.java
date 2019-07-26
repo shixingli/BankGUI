ef153060b579f07b0f1cb6a20dfb46e2300d4595
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2019-07-26 10:43:21 -0400

    reformatted w/ spelling and indents

--- Account.java
+++ Account.java
@@ -1,32 +1,32 @@
 abstract class Account {
     double balance;
     String[] txns;
-
+    
     void withDraw(double amount){
-  	    this.balance -= amount;
-  	  }
+      this.balance -= amount;
+    }
+    
+    void deposit(double amount){
+      this.balance += amount;
+    }
+    
+    double view_balance() {
+      return balance;
+    }
+    
+    String view_txns() {
+      return "";
+    }
 
-	  void deposite(double amount){
-	    this.balance += amount;
-	  }
-
-	  double view_balance() {
-		 return balance;
-	  }
-
-	  String view_txns() {
-		 return "";
-	  }
-
-	  <T> void transfer(T account, double amount){
-	  //transfer money from current account to saving/checking
-		//lets not implement right now since it is not required,
-	  }
+   <T> void transfer(T account, double amount){
+   //transfer money from current account to saving/checking
+  //lets not implement right now since it is not required,
+   }
 
 
-  	public static void main(String[] args) {
+   public static void main(String[] args) {
       AccountFrame window = new AccountFrame();
-		  window.accountframe.setVisible(true);
+    window.accountframe.setVisible(true);
     }
 
 
