0e5b066e551e543a1229cecba840fb4d66c8b3e5
Louis Tannudin <ltannudin@gmail.com>
2019-07-26 15:52:07 -0400

    Create the base for Manager GUI

--- Manager.java
+++ Manager.java
@@ -2,16 +2,25 @@ import javax.swing.*;
 
 public class Manager extends User {
   // methods like pass in a customer and get their information using the customer's getting methods
-    private class ManagerFrame extends JFrame {// GUI for the Manager part. Inner class for encapsulation
-
+    private class ManagerFrame extends JFrame {// GUI for the Manager part. Inner class for encapsulation since no other classes should be able to access this part???
+      public ManagerFrame(){
+        JFrame frame = new JFrame();
+        frame.setTitle("Manager Frame");
+        frame.setSize(450,250);
+        frame.setLocation(200,100);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+      }
     }
     public Manager(){
-
+      ManagerFrame mf = new ManagerFrame();
     }
 
-    public Customer getCustomer(Customer c){
+    public Customer getCustomer(String id){
         return null;
     }
 
-
+    public static void main(String[] args){
+      Manager m = new Manager();
+    }
 }
