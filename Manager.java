7fa25e116c734ceebc3e376332f947eb4440ee6d
Louis Tannudin <ltannudin@gmail.com>
2019-07-26 13:26:29 -0400

    Changes to the User and Manager class. Created user constructor. Initializing attributes of pin and id in the User abstract class

--- Manager.java
+++ Manager.java
@@ -1,7 +1,8 @@
+import javax.swing.*;
+
 public class Manager extends User {
   // methods like pass in a customer and get their information using the customer's getting methods
-
-    private class ManagerFrame {// GUI for the Manager part.
+    private class ManagerFrame extends JFrame {// GUI for the Manager part. Inner class for encapsulation
 
     }
     public Manager(){
--- User.java
+++ User.java
@@ -1,5 +1,20 @@
-abstract class User {
-  String id;
-  int pin;
-  
+public abstract class User {
+  private String id;
+  private int pin;
+
+  public User(String id, int pin){// Since User is an abstract class we should pass in the id and pin here no?
+    this.id = id;
+    this.pin = pin;
+  }
+
+  public User(){
+  }
+
+  public int getPin() {
+    return pin;
+  }
+
+  public String getId() {
+    return id;
+  }
 }
