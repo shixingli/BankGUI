5229b34e61a6ae49eca063102dbb60e1d528a12f
realmazafafa <jasonlishizhen@hotmail.com>
2019-07-29 11:03:36 -0400

    update manager interest rate

--- Bank.java
+++ Bank.java
@@ -7,34 +7,34 @@ public class Bank {
   public static Currency[] currencies;
   BankFrame frame;
   // ManagerFrame authorized; // manager gui interactivity
-  
+  static double interestrate;
   // Rich Man's Bank Default
   public Bank() {
     defaultCurrencies();
   }
-  
+
   // Rich Man's Bank w/ customers input
   public Bank(HashMap<String, Customer> users) {
     this();
     customers = users;
   }
-  
+
   public Bank(Currency[] inpCurrencies, HashMap<String, Customer> users) {
     Bank.newCurrencies(inpCurrencies);
     customers = users;
   }
-    
+
   public static void defaultCurrencies() {
     Currency USD = new Currency();
     Currency Bitcoin = new Currency("BIT", 9536.68);
-    Currency CAD = new Currency("CAD", 0.76);                          
+    Currency CAD = new Currency("CAD", 0.76);
     currencies = new Currency[]{USD, Bitcoin, CAD};
   }
-  
+
   public static void newCurrencies(Currency[] newCurr) {
     currencies = newCurr;
   }
-  
+
   void open(){
     frame = new BankFrame();
     frame.bankframe.setVisible(true);
@@ -44,17 +44,19 @@ public class Bank {
       Bank mybank = new Bank(Bank.test());
       mybank.open();
   }
-  
+  public static double (){
+    interestrate = Manager.getInterest();
+  }
   public static HashMap<String, Customer> test() {
     HashMap<String, Customer> map = new HashMap<String, Customer>();
-    
+
     Checking c1c = new Checking(101);
     Savings c1s = new Savings(11);
-    
+
     Checking c2c = new Checking(50);
-    
+
     Savings c3s = new Savings(1000000);
-    
+
     List<Account> c1Acc = new LinkedList<Account>();
     c1Acc.add(c1c);
     c1Acc.add(c1s);
@@ -64,11 +66,11 @@ public class Bank {
     map.put(c2.getUsername(), c2);
     Customer c3 = new Customer(c3s, "Jake Gyllenhaal", "deadpool", "48");
     map.put(c3.getUsername(), c3);
-   
+
     return map;
   }
-  
-  
-  
+
+
+
 
 }
